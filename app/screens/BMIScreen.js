f049c8cca9e94557ad27251d8613b9653ace719c
aditigupta16 <gaditi80@gmail.com>
2018-12-02 21:38:09 +0530

    Input text and button added

--- app/screens/BMIScreen.js
+++ app/screens/BMIScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, Button } from 'react-native';
 
 export default class BMIScreen extends React.Component {
+
+  handlePress() {
+    Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Your BMI calculator. Coming Soon!</Text>
+          <TextInput
+            style={{height: 40}}
+            placeholder="Enter your weight (pounds)"
+            />
+           <Text> Weight </Text>
+           <TextInput
+             style={{height: 40}}
+             placeholder="Enter your height (cms)"
+             />
+            <Text> Height </Text>
+            <Button
+            onPress={this.handlePress}
+            title="Calculate"
+
+          />
       </View>
     );
   }
