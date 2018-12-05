68ef6cbd591a9a21bd20058f75f302fb57865daf
aditigupta16 <gaditi80@gmail.com>
2018-12-05 20:05:02 +0530

    fetch call added

--- app/screens/BMIScreen.js
+++ app/screens/BMIScreen.js
@@ -1,30 +1,52 @@
-import React from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { Text, View, TextInput, Button } from "react-native";
 
 export default class BMIScreen extends React.Component {
+  handlePress = (weight, height) => {
+    fetch("http://localhost:8000/api/bmi-calculator/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        weight: this.state.weight,
+        height: this.state.height
+      })
+    })
+      .then(response => response.json())
 
-  handlePress() {
-    Alert.alert('You tapped the button!')
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: "",
+      weight: "",
+      error: false
+    };
+
+    this.handlePress = this.handlePress.bind(this);
   }
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <TextInput
-            style={{height: 40}}
-            placeholder="Enter your weight (pounds)"
-            />
-           <Text> Weight </Text>
-           <TextInput
-             style={{height: 40}}
-             placeholder="Enter your height (cms)"
-             />
-            <Text> Height </Text>
-            <Button
-            onPress={this.handlePress}
-            title="Calculate"
-
-          />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter your weight (pounds)"
+          onChangeText={weight => this.setState({ weight })}
+        />
+        <Text> Weight </Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter your height (cms)"
+          onChangeText={height => this.setState({ height })}
+        />
+        <Text> Height </Text>
+        <Button onPress={this.handlePress} title="Calculate" />
       </View>
     );
   }
