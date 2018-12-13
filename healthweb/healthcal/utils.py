5ce68e59f00df0ba5b8c563920fe7ecd8d5c97bb
aditigupta16 <gaditi80@gmail.com>
2018-12-13 19:49:51 +0530

    cleanup

---
+++ healthweb/healthcal/utils.py
@@ -0,0 +1,7 @@
+
+
+def get_weight_in_pounds(weight):
+	return float(weight)*2.205
+
+def get_height_in_inches(height):
+	return 0.393701*float(height)
--- healthweb/healthcal/views.py
+++ healthweb/healthcal/views.py
@@ -8,19 +8,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+from utils import get_weight_in_pounds, get_height_in_inches
+
 
 
 class BMICalculatorAPI(APIView):
 
 	def post(self, request, *args, **kwargs):
-		weight = float(request.data['weight'])/2.2
+		weight = float(request.data['weight'])
 		height = float(request.data['height'])/100
 		bmi = (weight/height)/height
+		bmi = str(round(bmi, 1))
 		return Response(data={'bmi':bmi}, status=status.HTTP_200_OK)
 
 
 class BMRCalculatorAPI(APIView):
-
+	# this is the formula for BMR. the units are converted accordingly after they are received from client
 	# Women: BMR = 655 + ( 4.35 x weight in pounds ) + ( 4.7 x height in inches ) - ( 4.7 x age in years )
 	# Men: BMR = 66 + ( 6.23 x weight in pounds ) + ( 12.7 x height in inches ) - ( 6.8 x age in years )
 
@@ -32,13 +35,15 @@ class BMRCalculatorAPI(APIView):
 					  }
 
 	def post(self, request, *args, **kwargs):
-		weight = float(request.data['weight'])
-		height = 0.393701*float(request.data['height'])
+		print(request.data)
+		weight = get_weight_in_pounds(request.data['height'])
+		height = get_height_in_inches(request.data['weight'])
 		age = float(request.data['age'])
 		gender = request.data['gender']
-		if gender == 'men':
+		if gender == 'male':
 			bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age)
 		else:
 			bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age)
 		total_calories_required = int(bmr * self.activity_factor[request.data['activity']])
+		total_calories_required = str(round(total_calories_required, 1))
 		return Response(data={'total_calories_required': total_calories_required}, status=status.HTTP_200_OK)
