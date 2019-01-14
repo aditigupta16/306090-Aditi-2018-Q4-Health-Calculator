9e6847450d5b8aa1766176ec6d3ff82a13dbdea0
aditigupta16 <gaditi80@gmail.com>
2019-01-14 12:25:52 +0530

    Weight target api added

--- healthweb/healthcal/urls.py
+++ healthweb/healthcal/urls.py
@@ -4,4 +4,5 @@ from healthweb.healthcal import views
 urlpatterns = [
     url(r'^bmi-calculator/$', views.BMICalculatorAPI.as_view()),
     url(r'^bmr-calculator/$', views.BMRCalculatorAPI.as_view()),
+    url(r'^weight-target-calculator/$', views.WeightTargetAPI.as_view()),
     ]
--- healthweb/healthcal/views.py
+++ healthweb/healthcal/views.py
@@ -36,8 +36,8 @@ class BMRCalculatorAPI(APIView):
 
 	def post(self, request, *args, **kwargs):
 		print(request.data)
-		weight = get_weight_in_pounds(request.data['height'])
-		height = get_height_in_inches(request.data['weight'])
+		weight = get_weight_in_pounds(request.data['weight'])
+		height = get_height_in_inches(request.data['height'])
 		age = float(request.data['age'])
 		gender = request.data['gender']
 		if gender == 'male':
@@ -47,3 +47,33 @@ class BMRCalculatorAPI(APIView):
 		total_calories_required = int(bmr * self.activity_factor[request.data['activity']])
 		total_calories_required = str(round(total_calories_required, 1))
 		return Response(data={'total_calories_required': total_calories_required}, status=status.HTTP_200_OK)
+
+
+class WeightTargetAPI(APIView):
+	#API that returns the calories user should consume in a day to achieve the desired weight loss
+
+    activity_factor = {'sedentary': 1.2, 
+                       'lightly active' : 1.375,
+                       'moderately active': 1.55,
+                       'very active': 1.725,
+                       'extra active': 1.9
+                      }	
+
+    def post(self, request, *args, **kwargs):
+    	import ipdb;ipdb.set_trace()
+        weight = get_weight_in_pounds(request.data['weight'])
+        target_weight = get_weight_in_pounds(request.data['target'])
+        height = get_height_in_inches(request.data['height'])
+        days = float(request.data['days'])
+        age = float(request.data['age'])
+        gender = request.data['gender']
+        if gender == 'male':
+            bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age)
+        else:
+            bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age)
+        total_calories_required = int(bmr * self.activity_factor[request.data['activity']])
+        # total_calories_required = int(round(total_calories_required, 1))        
+        calories_required_to_lose_per_day = (3500*2*(weight-target_weight))/days
+        calories_required_to_consume = total_calories_required - calories_required_to_lose_per_day
+        calories_required_to_consume = int(round(calories_required_to_consume, 1))
+        return Response(data={'calories_required_to_consume: calories_required_to_consume'}, status=status.HTTP_200_OK)
